a441d0fadcfb6b9d6db3e4d1c4498bfeaa297603
santoshnainala <51361959+santoshnainala@users.noreply.github.com>
2022-11-03 22:39:06 +0530

    Add files via upload

---
+++ weight_in_pounds.py
@@ -0,0 +1,4 @@
+name = input("Enter your name : \n")
+weight = int(input("enter your weight in kgs : \n"))
+pounds = weight/0.45359237
+print("hello "+name+", your weight in pounds is "+str(pounds)+" lbs")
